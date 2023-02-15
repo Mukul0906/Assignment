0afcc27a3d70529d30cb3d80ccb7022cbe66cce7
Mukul0906 <122787724+Mukul0906@users.noreply.github.com>
2023-02-15 16:43:37 +0530

    Add files via upload

---
+++ Java Activity/AreaCalculator.java
@@ -0,0 +1,13 @@
+package com.cognizant.shapes;
+
+class Rectangle{
+	void calculateArea(int l,int b){
+		System.out.println("The Area of the rectangle is calculated using the formula length * breadth");
+}
+}
+public class AreaCalculator {
+	public static void main(String[] args) {
+        Rectangle ar = new Rectangle();
+        ar.calculateArea(15, 20);	
+	}
+}
---
+++ Java Activity/DisplayMessage.java
@@ -0,0 +1,6 @@
+class DisplayMessage{
+ public static void main(String[] args){
+  void printMessage(String a){
+   String a="Hello My Message";
+}
+}  
